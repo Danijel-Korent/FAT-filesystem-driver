4c259cb735a082aee56ff47bdee20ea4bd800e9b
Danijel Korent <danijel.korent@gmail.com>
2018-10-28 14:27:34 +0100

    Some more skeleton code and updated FAT structure comments

--- source_code/FAT-filesystem-reader.c
+++ source_code/FAT-filesystem-reader.c
@@ -8,26 +8,30 @@ static void print_root_files(void)
 {
     // TODO: put all necessary logic here, and then split it into functional parts
 
+    // https://en.wikipedia.org/wiki/Design_of_the_FAT_file_system
 
     /*************************************************************************************
     * OVERALL STRUCTURE OF FAT FS
     *
-    *  Boot sector (1sector/512bytes)
-    *  Reserved area
+    *  RESERVED SECTORS
+    *      - VBR - Volume boot record / partition boot sector (1sector/512bytes)
+    *      - FS Information Sector (FAT32 only)
+    *      - Rest of reserved sectors
     *  FAT TABLE #1
-    *  FAT TABLE #2
-    *  ROOT DIRECTORY AREA
+    *  FAT TABLE #2        (optional)
+    *  ROOT DIRECTORY AREA (FAT12/16 only)
     *  DATA AREA
     *       - Cluser_01
-    *       - Cluser_xy
+    *       - Cluser_xy -> until the end of partition
     *
     */
 
     /*************************************************************************************
-    * OVERALL STRUCTURE OF BOOT SECTOR - the first 512 bytes of a FS (not MBR)
+    * OVERALL STRUCTURE OF Volume Boot Record (VBR) - the first 512 bytes of a FS (not MBR)
     *
-    *     0 -   3  Boot code jump instruction
-    *     4 -  37  FAT parameters ??
+    *     0 -   2  Boot code jump instruction
+    *     3 -   8  OEM Name string
+    *     9 -  37  BIOS Parametar block ??
     *    90 - 510  Boot code (bootloader) ??
 
     *   511-512 - Boot sector signature
@@ -62,6 +66,18 @@ static void run_pseudo_shell(void)
         {
             break;
         }
+        else if (0 == user_input[0])
+        {
+            // Empty input - do nothing
+        }
+        else if ('c' == user_input[0] && 'd' == user_input[1]) // cd
+        {
+            execute_command_cd(NULL, 0);
+        }
+        else if ('l' == user_input[0] && 's' == user_input[1]) // ls
+        {
+            execute_command_ls(NULL, 0);
+        }
         else
         {
             printf("%s: Unknown command", user_input);
@@ -71,6 +87,19 @@ static void run_pseudo_shell(void)
     }
 }
 
+static void execute_command_cd(const char *args, int args_lenght)
+{
+    printf("cd unimplemented");
+}
+
+static void execute_command_ls(const char *args, int args_lenght)
+{
+    printf(" ls example output: \n\n");
+
+    printf(" %s %06i  %s \n", "d", 0, "DIR_5");
+    printf(" %s %06i  %s \n", "f", 5, "FILE_6");
+    printf(" %s %06i  %s \n", "f", 255, "FILE_7");
+}
 
 int main(void)
 {
