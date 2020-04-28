b4b9da70d1ce3103b68ffefa0043d06ce8175d90
Danijel Korent <danijel.korent@gmail.com>
2020-04-28 20:19:42 +0200

    Added TODO for data dump; Done some var name refactoring

--- source_code/FAT-filesystem-reader.c
+++ source_code/FAT-filesystem-reader.c
@@ -900,30 +900,33 @@ void print_all_clusters_info(void)
 
 void dump_data(void)
 {
-    printf("Placeholder for dump data \n");
+    printf("\nFAT IMAGE DATA DUMP: \n");
 
     // TODO:
-    //      - add into a loop and print at least 10 rows
-    //      - make it print 16 bytes per row, just like hex dump
+    //      - add support for 'address' argument as a number
+    //      - add support for argument:
+    //          - boot      -> prints data from boot/VBR sector
+    //          - FAT       -> prints data of fat table
+    //          - cluster n -> prints data of cluster n
 
-    //const unsigned char* const FS_image = FS_image; // Why 'p'? I don't know
+    const unsigned char* const data = FS_image;
 
-    int it = 0;
+    int base = 0;
 
     for (int row = 0; row <= 20; row++)
     {
-        printf("\n %04d: ", it);
-        printf("%02x %02x %02x %02x ", FS_image[it+0], FS_image[it+1], FS_image[it+2], FS_image[it+3]);
-        printf("%02x %02x %02x %02x ", FS_image[it+4], FS_image[it+5], FS_image[it+6], FS_image[it+7]);
+        printf("\n %04d: ", base);
+        printf("%02x %02x %02x %02x ", data[base+0], data[base+1], data[base+2], data[base+3]);
+        printf("%02x %02x %02x %02x ", data[base+4], data[base+5], data[base+6], data[base+7]);
         printf(" ");
-        printf("%02x %02x %02x %02x ", FS_image[it+8], FS_image[it+9], FS_image[it+10], FS_image[it+11]);
-        printf("%02x %02x %02x %02x ", FS_image[it+12], FS_image[it+13], FS_image[it+14], FS_image[it+15]);
+        printf("%02x %02x %02x %02x ", data[base+8], data[base+9], data[base+10], data[base+11]);
+        printf("%02x %02x %02x %02x ", data[base+12], data[base+13], data[base+14], data[base+15]);
         printf("  ");
         printf("|");
 
         for(int char_no = 0; char_no < 16; char_no++)
         {
-            char character = FS_image[it+char_no];
+            char character = data[base+char_no];
 
             // Replace control characters with a dot
             if (character < 32) character = '.';
@@ -931,6 +934,6 @@ void dump_data(void)
             printf("%c", character); //TODO APPETIZER: find function for outputing single char
         }
         printf("|");
-        it += 16;
+        base += 16;
     }
 }
